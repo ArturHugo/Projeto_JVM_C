94fa39f8bb1cb5a6e150126692bf7a1410ef91b6
artur zorron <tucazorron@gmail.com>
2021-04-07 01:40:43 -0300

    implement add, sub, mul, div, rem and neg

---
+++ src/math-instructions.c
@@ -0,0 +1,95 @@
+int iadd(value1, value2) {
+    return value1 + value2;
+}
+
+long ladd(value1, value2) {
+    return value1 + value2;
+}
+
+float fadd(value1, value2) {
+    return value1 + value2;
+}
+
+double daad(value1, value2) {
+    return value1 + value2;
+}
+
+int isub(value1, value2) {
+    return value1 - value2;
+}
+
+long lsub(value1, value2) {
+    return value1 - value2;
+}
+
+float fsub(value1, value2) {
+    return value1 - value2;
+}
+
+double dsub(value1, value2) {
+    return value1 - value2;
+}
+
+int imul(value1, value2) {
+    return value1 * value2;
+}
+
+long lmul(value1, value2) {
+    return value1 * value2;
+}
+
+float fmul(value1, value2) {
+    return value1 * value2;
+}
+
+double dmul(value1, value2) {
+    return value1 * value2;
+}
+
+int idiv(value1, value2) {
+    return value1 / value2;
+}
+
+long ldiv(value1, value2) {
+    return value1 / value2;
+}
+
+float fdiv(value1, value2) {
+    return value1 / value2;
+}
+
+double ddiv(value1, value2) {
+    return value1 / value2;
+}
+
+int irem(value1, value2) {
+    return value1 - (value1 / value2) * value2;
+}
+
+long lrem(value1, value2) {
+    return value1 - (value1 / value2) * value2;
+}
+
+float frem(value1, value2) {
+    return value1 - (value1 / value2) * value2;
+}
+
+double drem(value1, value2) {
+    return value1 - (value1 / value2) * value2;
+}
+
+int ineg(value) {
+    return 0 - value;
+}
+
+long lneg(value) {
+    return 0 - value;
+}
+
+float fneg(value) {
+    return 0 - value;
+}
+
+double dneg(value) {
+    return 0 - value;
+}
