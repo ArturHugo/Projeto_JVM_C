ae2e2fd9d46097b5c9fc184f1c6a50ff3db539a8
Felipe Oliveira Magno Neves <felipe.neves16@gmail.com>
2021-04-12 19:47:52 -0300

    Update common.h

--- include/common.h
+++ include/common.h
@@ -32,16 +32,22 @@ typedef uint32_t u4;
 #define NAN_MASK_F  0x007FFFFF
 #define NAN_MASK_D  0x000FFFFFFFFFFFFF
 
-typedef union JavaType {
-  int8_t   byte_value;
-  int16_t  short_value;
-  int32_t  int_value;
-  int64_t  long_value;
-  uint16_t char_value;
-  float    float_value;
-  double   double_value;
-  void*    reference_value;
-  uint32_t return_address_value;
+#define CAT1 1
+#define CAT2 2
+
+typedef struct JavaType {
+  u1 cat_tag;
+  union {
+    int8_t   byte_value;
+    int16_t  short_value;
+    int32_t  int_value;
+    int64_t  long_value;
+    uint16_t char_value;
+    float    float_value;
+    double   double_value;
+    void*    reference_value;
+    uint32_t return_address_value;
+  };
 } JavaType;
 
 #endif  // __COMMON_H
