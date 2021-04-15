7d0819874187a0afd84a70164e5a5bfc5d6f95a1
gui <guilhermeoliveira.0210@gmail.com>
2021-04-15 15:41:26 -0300

    Arrumando .hcom assinaturas de metodos

--- include/handlers/comparison.h
+++ include/handlers/comparison.h
@@ -3,5 +3,24 @@
 
 #include "common.h"
 
+void lcmp();
+void fcmpl();
+void fcmpg();
+void dcmpl();
+void dcmpg();
+void ifeq();
+void ifne();
+void iflt();
+void ifge();
+void ifgt();
+void ifle();
+void if_icmpeq();
+void if_icmpne();
+void if_icmplt();
+void if_icmpge();
+void if_icmpgt();
+void if_icmple();
+void if_acmpeq();
+void if_acmpne();
 
 #endif  // __COMPARISON_H
