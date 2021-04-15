48cd8678444aec44f4b33840a24f5ccdb3ff86dd
Felipe Oliveira Magno Neves <felipe.neves16@gmail.com>
2021-04-15 09:27:32 -0300

    adicionando constant_pool_count a frame

--- src/frame.c
+++ src/frame.c
@@ -3,11 +3,12 @@
 #include "class-file.h"
 
 Frame* newFrame(ClassFile* current_class, u2 method_index) {
-  Frame* new_frame          = malloc(sizeof(*new_frame));
-  new_frame->constant_pool  = current_class->constant_pool;
-  new_frame->current_method = &(current_class->methods[method_index]);
-  new_frame->operand_stack  = NULL;
-  new_frame->local_pc       = 0;
+  Frame* new_frame               = malloc(sizeof(*new_frame));
+  new_frame->constant_pool_count = current_class->constant_pool_count;
+  new_frame->constant_pool       = current_class->constant_pool;
+  new_frame->current_method      = &(current_class->methods[method_index]);
+  new_frame->operand_stack       = NULL;
+  new_frame->local_pc            = 0;
   // TODO pode não ser attribute[0] que contém code info
   new_frame->local_variables =
       malloc((new_frame->current_method->attributes->code_info.max_locals) * sizeof(u4));
