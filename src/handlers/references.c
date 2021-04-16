341ee6cf6a05805f93045b339fcf1560b2c48234
Felipe Oliveira Magno Neves <felipe.neves16@gmail.com>
2021-04-16 15:31:09 -0300

    finishing invokestatic

--- src/handlers/references.c
+++ src/handlers/references.c
@@ -10,7 +10,6 @@ void invokestatic(const u1* instruction) {
   char* new_method_name;
   char* new_method_descriptor;
 
-  // TODO does this even work?
   if(current_cp_info->tag == CONSTANT_METHODREF) {
     new_class_name        = (char*) (current_cp_info->methodref_info._class);
     new_method_name       = (char*) (current_cp_info->methodref_info._name);
@@ -21,12 +20,13 @@ void invokestatic(const u1* instruction) {
     new_method_descriptor = (char*) (current_cp_info->interface_methodref_info._descriptor);
   }
 
+  // TODO does this even work?
   loadClass(new_class_name);
 
   ClassFile* new_class = mapGet(method_area.loaded_classes, new_class_name);
 
   if(new_class == NULL) {
-    printf("\npc = %d: invokestatic failed\n");
+    printf("\npc = %d: invokestatic failed\n", current_frame->local_pc);
     exit(1);
   }
 
@@ -35,13 +35,12 @@ void invokestatic(const u1* instruction) {
 
   Frame* new_frame = newFrame(new_class, new_method_name);
 
-  MethodInfo* new_method = mapGet(new_class->_method_map, new_method_name);
-
-  // TODO calcular n args a partir de new_method_descriptor
-  u2 n_args = 0;
+  u2 n_args = getArgumentCount((u1*) new_method_descriptor);
 
   u2       local_variable_index = 0;
   JavaType temp_value;
+
+  // loading opperands from current stack to new local variables
   for(u2 i = 0; i < n_args; i++) {
     popValue(&(current_frame->operand_stack), &temp_value);
     new_frame->local_variables[local_variable_index] = temp_value;
--- src/main.c
+++ src/main.c
@@ -17,49 +17,47 @@ MethodArea method_area;
 Stack      frame_stack;
 
 int main(int numargs, char* arg[]) {
-  /*
-    if(numargs != 3) {
-      printf("Número errado de argumentos. Faça ./output/main.exe -<flag> <nome do
-    arquivo.class>\n"); printf("Flag -l para leitor-exibidor\n"); printf("Flag -i para
-    interpretador\n"); return 1;
+
+  if(numargs != 3) {
+    printf("Número errado de argumentos. Faça ./output/main.exe -<flag> <nome do arquivo.class>\n");
+    printf("Flag -l para leitor-exibidor\n");
+    printf("Flag -i para interpretador\n");
+    return 1;
+  }
+
+  if(!strcmp(arg[1], "-l")) {
+    FILE* file = fopen(arg[2], "rb");
+
+    File* fd = convertFile(file);
+
+    fclose(file);
+
+    ClassFile* class_file = readClassFile(fd);
+
+    char* source_file_name = strtok(getSourceFile(class_file), ".");
+    char* class_file_name  = strtok(arg[2], ".");
+
+    if(strcmp(source_file_name, class_file_name)) {
+      printf("Erro: nome do source file e do class file sao diferentes!\n");
+      return 0;
     }
 
-    if(!strcmp(arg[1], "-l")) {
-      FILE* file = fopen(arg[2], "rb");
+    printClassFile(class_file);
 
-      File* fd = convertFile(file);
+    free(fd->buffer);
+    free(fd);
+  } else if(!strcmp(arg[1], "-i")) {
+    method_area.loaded_classes = newMap();
 
-      fclose(file);
+    loadObjectClass();
+    loadClass(arg[2]);
 
-      ClassFile* class_file = readClassFile(fd);
+    char* class_name = trimSuffix(arg[2], ".class");
+    Class* class     = mapGet(method_area.loaded_classes, class_name);
+    resolveReferences(class);
 
-      char* source_file_name = strtok(getSourceFile(class_file), ".");
-      char* class_file_name  = strtok(arg[2], ".");
-
-      if(strcmp(source_file_name, class_file_name)) {
-        printf("Erro: nome do source file e do class file sao diferentes!\n");
-        return 0;
-      }
-
-      printClassFile(class_file);
-
-      free(fd->buffer);
-      free(fd);
-    } else if(!strcmp(arg[1], "-i")) {
-      method_area.loaded_classes = newMap();
-
-      loadObjectClass();
-      loadClass(arg[2]);
-
-      char* class_name = trimSuffix(arg[2], ".class");
-      Class* class     = mapGet(method_area.loaded_classes, class_name);
-      resolveReferences(class);
-
-      run(class_name);
-      free(class_name);
-    }*/
-  char descriptor[] = "(IDLjava/qualquercoisa;[[[ZIS)Ljava/lang/Object;";
-
-  printf("O descritor %s tem %d arqumentos\n", descriptor, getArgumentCount((u1*) descriptor));
+    run(class_name);
+    free(class_name);
+  }
   return (0);
 }
