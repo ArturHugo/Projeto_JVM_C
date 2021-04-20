d5a92883064b3c23b2c888ea69b93763275bf761
gui <guilhermeoliveira.0210@gmail.com>
2021-04-20 20:42:02 -0300

    Atualizar tipo para int8_t. Return_op com pop_node. Verificar os free() nas instruções de retorno

--- src/handlers/control.c
+++ src/handlers/control.c
@@ -7,10 +7,10 @@
 
 void go_to(){
     Frame* current_frame = peekNode(frame_stack);  
-    uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
+    int8_t* bytecode = current_frame->current_method->attributes->code_info.code;
 
-    uint8_t byte1 = bytecode[current_frame->local_pc + 1];
-    uint8_t byte2 = bytecode[current_frame->local_pc + 2];
+    int8_t byte1 = bytecode[current_frame->local_pc + 1];
+    int8_t byte2 = bytecode[current_frame->local_pc + 2];
     int16_t branchOffset = ((int16_t)byte1 << 8) | byte2;
 
     current_frame->local_pc = current_frame->local_pc + branchOffset;
@@ -18,11 +18,11 @@ void go_to(){
 
 void goto_w(){
     Frame* current_frame = peekNode(frame_stack);  
-    uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
-    uint8_t byte1 = bytecode[current_frame->local_pc + 1];
-    uint8_t byte2 = bytecode[current_frame->local_pc + 2];
-    uint8_t byte3 = bytecode[current_frame->local_pc + 3];
-    uint8_t byte4 = bytecode[current_frame->local_pc + 4];
+    int8_t* bytecode = current_frame->current_method->attributes->code_info.code;
+    int8_t byte1 = bytecode[current_frame->local_pc + 1];
+    int8_t byte2 = bytecode[current_frame->local_pc + 2];
+    int8_t byte3 = bytecode[current_frame->local_pc + 3];
+    int8_t byte4 = bytecode[current_frame->local_pc + 4];
     int16_t branchOffset = ((int16_t)byte1 << 24) | ((int16_t)byte2 << 16) | ((int16_t)byte3 << 8) | ((int16_t)byte4 << 0)   ;
 
     current_frame->local_pc = current_frame->local_pc + branchOffset;
@@ -30,10 +30,10 @@ void goto_w(){
 
 void jsr(){
     Frame* current_frame = peekNode(frame_stack);  
-    uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
+    int8_t* bytecode = current_frame->current_method->attributes->code_info.code;
 
-    uint8_t byte1 = bytecode[current_frame->local_pc + 1];
-    uint8_t byte2 = bytecode[current_frame->local_pc + 2];
+    int8_t byte1 = bytecode[current_frame->local_pc + 1];
+    int8_t byte2 = bytecode[current_frame->local_pc + 2];
     uint16_t branchOffset = ((int16_t)byte1 << 8) | byte2;
     JavaType return_address;
 
@@ -45,8 +45,8 @@ void jsr(){
 
 void ret(){
     Frame* current_frame = peekNode(frame_stack);  
-    uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
-    uint8_t index = bytecode[current_frame->local_pc + 1];
+    int8_t* bytecode = current_frame->current_method->attributes->code_info.code;
+    int8_t index = bytecode[current_frame->local_pc + 1];
 
     JavaType value ;
     value = current_frame->local_variables[index];
@@ -105,7 +105,7 @@ void areturn(){
 }
 
 void return_op(){
-	JavaType value;	
-	Frame* current_frame = peekNode(frame_stack);
-	popValue(&current_frame -> operand_stack, &value);
+	popNode(&frame_stack);
+	//free(operand_stack);
+	//free(previous_node);
 }
