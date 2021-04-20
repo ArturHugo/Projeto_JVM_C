4ebef334b42042f96e73b2f3fe9d55d8d1ea24c8
Felipe Oliveira Magno Neves <felipe.neves16@gmail.com>
2021-04-20 18:47:41 -0300

    Update execution-engine.h

--- include/execution-engine.h
+++ include/execution-engine.h
@@ -1,20 +1,6 @@
 #ifndef __EXECUTION_ENGINE_H
 #define __EXECUTION_ENGINE_H
 
-#include "class-file.h"
-#include "common.h"
-#include "frame.h"
-#include "global.h"
-
-#include "handlers/constants.h"
-#include "handlers/conversions.h"
-#include "handlers/fields.h"
-#include "handlers/loads.h"
-#include "handlers/references.h"
-#include "handlers/stack.h"
-#include "handlers/stores.h"
-
-
 extern void (*const instructions_handlers[256])(const u1*);
 void run(char* start_class_name);
 
