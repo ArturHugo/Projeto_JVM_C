3e2684807fa8c5a1e19d9438bc3440a907395e8c
guioliunb <52641472+guioliunb@users.noreply.github.com>
2021-04-23 20:02:32 -0300

    Add files via upload
    Tableswitch e Lookupswitch criadas. Adicionada documentação.

--- src/handlers/comparison.c
+++ src/handlers/comparison.c
@@ -5,6 +5,16 @@
 #include "global.h"
 #include "handlers/comparison.h"
 
+/*
+ * Compare long
+ *
+ * opcode:  0x94
+ * format:  [lcmp]
+ * stack:   (..., value1, value2) -> (..., result)
+ * description: takes two values of type long from operand stack
+ * and pushes into the operand stack..
+ */
+
 void lcmp(){
     JavaType value1;
     JavaType value2;
@@ -28,6 +38,15 @@ void lcmp(){
     current_frame -> local_pc ++;
 }
 
+/*
+ * Compare float
+ *
+ * opcode:  0x95
+ * format:  [fcmp<op>]
+ * stack:   (..., value1, value2) -> (..., result)
+ * description: takes two values of type float from operand stack
+ * and pushes into the operand stack..
+ */
 void fcmpl(){
     JavaType value1;
     JavaType value2;
@@ -36,9 +55,6 @@ void fcmpl(){
     popValue(&current_frame -> operand_stack, &value2);
     popValue(&current_frame -> operand_stack, &value1);
 
-    //VERIFICAR SE NO POP JA CONSEGUE PEGAR O FLOAT VALUE OU PEGAR 
-    // INT E FAZER CONVERSAO 
-
     if (value1.float_value > value2.float_value) {
         result.cat_tag = CAT1;
         result.int_value = (int32_t)1;
@@ -48,12 +64,24 @@ void fcmpl(){
     } else if (value1.float_value < value2.float_value) {
         result.cat_tag = CAT1;
         result.int_value = (int32_t)-1;
+    } else {
+        result.cat_tag = CAT1;
+        result.int_value = (int32_t)1;  
     }
 
     pushValue(&current_frame -> operand_stack, result);
     current_frame -> local_pc ++;
 }
 
+/*
+ * Compare float
+ *
+ * opcode:  0x96
+ * format:  [fcmp<op>]
+ * stack:   (..., value1, value2) -> (..., result)
+ * description: takes two values of type float from operand stack
+ * and pushes into the operand stack..
+ */
 void fcmpg(){
     JavaType value1;
     JavaType value2;
@@ -62,9 +90,6 @@ void fcmpg(){
     popValue(&current_frame -> operand_stack, &value2);
     popValue(&current_frame -> operand_stack, &value1);
 
-    //VERIFICAR SE NO POP JA CONSEGUE PEGAR O FLOAT VALUE OU PEGAR 
-    // INT E FAZER CONVERSAO pra FLOAT
-
     if (value1.float_value > value2.float_value) {
         result.cat_tag = CAT1;
         result.int_value = (int32_t)1;
@@ -74,12 +99,24 @@ void fcmpg(){
     } else if (value1.float_value < value2.float_value) {
         result.cat_tag = CAT1;
         result.int_value = (int32_t)-1;
+    } else {
+        result.cat_tag = CAT1;
+        result.int_value = (int32_t)1;  
     }
 
     pushValue(&current_frame -> operand_stack, result);
     current_frame -> local_pc ++;
 }
 
+/*
+ * Compare double
+ *
+ * opcode:  0x97
+ * format:  [dcmp<op>]
+ * stack:   (..., value1, value2) -> (..., result)
+ * description: takes two values of type double from operand stack
+ * and pushes into the operand stack..
+ */
 void dcmpl(){
     JavaType value1;
     JavaType value2;
@@ -103,6 +140,15 @@ void dcmpl(){
     current_frame -> local_pc ++;
 }
 
+/*
+ * Compare double
+ *
+ * opcode:  0x98
+ * format:  [dcmp<op>]
+ * stack:   (..., value1, value2) -> (..., result)
+ * description: takes two values of type double from operand stack
+ * and pushes into the operand stack..
+ */
 void dcmpg(){
     JavaType value1;
     JavaType value2;
@@ -126,6 +172,15 @@ void dcmpg(){
     current_frame -> local_pc ++;
 }
 
+/*
+ * Branch if int comparison with zero succeeds
+ *
+ * opcode:  0x99
+ * format:  [if<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value) -> (...)
+ * description: takes values of type int from operand stack
+ * and update pc value..
+ */
 void ifeq(){
     Frame* current_frame = peekNode(frame_stack);
     JavaType value;
@@ -142,6 +197,15 @@ void ifeq(){
     current_frame-> local_pc = current_frame->local_pc + 2 + 1;
 }
 
+/*
+ * Branch if int comparison with zero succeeds
+ *
+ * opcode:  0x9a
+ * format:  [if<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value) -> (...)
+ * description: takes values of type int from operand stack
+ * and update pc value..
+ */
 void ifne(){
     Frame* current_frame = peekNode(frame_stack);
     JavaType value;
@@ -158,6 +222,15 @@ void ifne(){
     current_frame-> local_pc = current_frame->local_pc + 2 + 1;
 }
 
+/*
+ * Branch if int comparison with zero succeeds
+ *
+ * opcode:  0x9b
+ * format:  [if<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value) -> (...)
+ * description: takes values of type int from operand stack
+ * and update pc value..
+ */
 void iflt(){
     Frame* current_frame = peekNode(frame_stack);
     JavaType value;
@@ -174,23 +247,15 @@ void iflt(){
     current_frame-> local_pc = current_frame->local_pc + 2 + 1;
 }
 
-void ifle(){
-    Frame* current_frame = peekNode(frame_stack);
-    JavaType value;
-    uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
-    uint32_t baseAddress = current_frame->local_pc;
-    uint8_t byte1 = bytecode[current_frame->local_pc + 1];
-    uint8_t byte2 = bytecode[current_frame->local_pc + 2];
-    int16_t branchOffset = ((int16_t)byte1 << 8) | byte2;
-
-    popValue(&current_frame -> operand_stack, &value);
-
-    if ((int32_t)value.int_value <= 0) {
-            current_frame->local_pc = baseAddress + branchOffset;
-    }
-    current_frame-> local_pc = current_frame->local_pc + 2 + 1;
-}
-
+/*
+ * Branch if int comparison with zero succeeds
+ *
+ * opcode:  0x9c
+ * format:  [if<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value) -> (...)
+ * description: takes values of type int from operand stack
+ * and update pc value..
+ */
 void ifge(){
     Frame* current_frame = peekNode(frame_stack);
     JavaType value;
@@ -208,6 +273,15 @@ void ifge(){
     current_frame-> local_pc = current_frame->local_pc + 2 + 1;
 }
 
+/*
+ * Branch if int comparison with zero succeeds
+ *
+ * opcode:  0x9d
+ * format:  [if<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value) -> (...)
+ * description: takes values of type int from operand stack
+ * and update pc value..
+ */
 void ifgt(){
     Frame* current_frame = peekNode(frame_stack);
     JavaType value;
@@ -224,8 +298,41 @@ void ifgt(){
     }
     current_frame-> local_pc = current_frame->local_pc + 2 + 1;
 }
+/*
+ * Branch if int comparison with zero succeeds
+ *
+ * opcode:  0x9e
+ * format:  [if<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value) -> (...)
+ * description: takes values of type int from operand stack
+ * and update pc value..
+ */
+void ifle(){
+    Frame* current_frame = peekNode(frame_stack);
+    JavaType value;
+    uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
+    uint32_t baseAddress = current_frame->local_pc;
+    uint8_t byte1 = bytecode[current_frame->local_pc + 1];
+    uint8_t byte2 = bytecode[current_frame->local_pc + 2];
+    int16_t branchOffset = ((int16_t)byte1 << 8) | byte2;
 
+    popValue(&current_frame -> operand_stack, &value);
 
+    if ((int32_t)value.int_value <= 0) {
+            current_frame->local_pc = baseAddress + branchOffset;
+    }
+    current_frame-> local_pc = current_frame->local_pc + 2 + 1;
+}
+
+/*
+ * Branch if int comparison succeeds
+ *
+ * opcode:  0x9f
+ * format:  [if_icmp<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value1, value2) -> (...)
+ * description: takes two values of type int from operand stack
+ * and update pc value..
+ */
 void if_icmpeq(){
     Frame* current_frame = peekNode(frame_stack);  
     uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
@@ -243,6 +350,16 @@ void if_icmpeq(){
     }
     current_frame->local_pc = current_frame->local_pc + 2 + 1;
 }
+
+/*
+ * Branch if int comparison succeeds
+ *
+ * opcode:  0xa0
+ * format:  [if_icmp<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value1, value2) -> (...)
+ * description: takes two values of type int from operand stack
+ * and update pc value..
+ */
 void if_icmpne(){
     Frame* current_frame = peekNode(frame_stack);  
     uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
@@ -261,6 +378,15 @@ void if_icmpne(){
     current_frame->local_pc = current_frame->local_pc + 2 + 1;  
 }
 
+/*
+ * Branch if int comparison succeeds
+ *
+ * opcode:  0xa1
+ * format:  [if_icmp<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value1, value2) -> (...)
+ * description: takes two values of type int from operand stack
+ * and update pc value..
+ */
 void if_icmplt(){
     Frame* current_frame = peekNode(frame_stack);  
     uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
@@ -278,6 +404,16 @@ void if_icmplt(){
     }
     current_frame->local_pc = current_frame->local_pc + 2 + 1;  
 }
+
+/*
+ * Branch if int comparison succeeds
+ *
+ * opcode:  0xa2
+ * format:  [if_icmp<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value1, value2) -> (...)
+ * description: takes two values of type int from operand stack
+ * and update pc value..
+ */
 void if_icmpge(){
     Frame* current_frame = peekNode(frame_stack);  
     uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
@@ -295,6 +431,16 @@ void if_icmpge(){
     }
     current_frame->local_pc = current_frame->local_pc + 2 + 1;   
 }
+
+/*
+ * Branch if int comparison succeeds
+ *
+ * opcode:  0xa3
+ * format:  [if_icmp<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value1, value2) -> (...)
+ * description: takes two values of type int from operand stack
+ * and update pc value..
+ */
 void if_icmpgt(){
     Frame* current_frame = peekNode(frame_stack);  
     uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
@@ -312,6 +458,16 @@ void if_icmpgt(){
     }
     current_frame->local_pc = current_frame->local_pc + 2 + 1;   
 }
+
+/*
+ * Branch if int comparison succeeds
+ *
+ * opcode:  0xa4
+ * format:  [if_icmp<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value1, value2) -> (...)
+ * description: takes two values of type int from operand stack
+ * and update pc value..
+ */
 void if_icmple(){
     Frame* current_frame = peekNode(frame_stack);  
     uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
@@ -329,6 +485,16 @@ void if_icmple(){
     }
     current_frame->local_pc = current_frame->local_pc + 2 + 1;   
 }
+
+/*
+ * Branch if reference comparison succeeds
+ *
+ * opcode:  0xa5
+ * format:  [if_acmp<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value1, value2) -> (...)
+ * description: takes two values of type reference from operand stack
+ * and update pc value..
+ */
 void if_acmpeq(){
     //testar
     Frame* current_frame = peekNode(frame_stack);  
@@ -347,6 +513,16 @@ void if_acmpeq(){
     }
     current_frame->local_pc = current_frame->local_pc + 2 + 1;
 }
+
+/*
+ * Branch if reference comparison succeeds
+ *
+ * opcode:  0xa6
+ * format:  [if_acmp<cond>, branchbyte1, branchbyte2]
+ * stack:   (..., value1, value2) -> (...)
+ * description: takes two values of type reference from operand stack
+ * and update pc value..
+ */
 void if_acmpne(){
     Frame* current_frame = peekNode(frame_stack);  
     uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
--- src/handlers/control.c
+++ src/handlers/control.c
@@ -5,9 +5,19 @@
 #include "global.h"
 #include "handlers/comparison.h"
 
+/*
+ * Branch always
+ *
+ * opcode:	0xc8
+ * format: 	[goto, branchbyte1, branchbyte2 ]
+ * stack: 	(...) -> (...)
+ * description:	takes pc value from current frame
+ * and update using branch offset.
+ */
+
 void go_to(){
     Frame* current_frame = peekNode(frame_stack);  
-    int8_t* bytecode = current_frame->current_method->attributes->code_info.code;
+    uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
 
     int8_t byte1 = bytecode[current_frame->local_pc + 1];
     int8_t byte2 = bytecode[current_frame->local_pc + 2];
@@ -16,9 +26,19 @@ void go_to(){
     current_frame->local_pc = current_frame->local_pc + branchOffset;
 }
 
+/*
+ * Branch always (wide index)
+ *
+ * opcode:	0xc8
+ * format: 	[goto, branchbyte1, branchbyte2, branchbyte3, branchbyte4 ]
+ * stack: 	(...) -> (...)
+ * description:	takes pc value from current frame
+ * and update using branch offset.
+ */
+
 void goto_w(){
     Frame* current_frame = peekNode(frame_stack);  
-    int8_t* bytecode = current_frame->current_method->attributes->code_info.code;
+    uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
     int8_t byte1 = bytecode[current_frame->local_pc + 1];
     int8_t byte2 = bytecode[current_frame->local_pc + 2];
     int8_t byte3 = bytecode[current_frame->local_pc + 3];
@@ -28,9 +48,18 @@ void goto_w(){
     current_frame->local_pc = current_frame->local_pc + branchOffset;
 }
 
+/*
+ * Jump subroutine
+ *
+ * opcode:	0xa8
+ * format: 	[jsr, branchbyte1, branchbyte2]
+ * stack: 	(...) -> (..., address)
+ * description:	takes pc value from current frame
+ * and pushes into the operand stack..
+ */
 void jsr(){
     Frame* current_frame = peekNode(frame_stack);  
-    int8_t* bytecode = current_frame->current_method->attributes->code_info.code;
+    uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
 
     int8_t byte1 = bytecode[current_frame->local_pc + 1];
     int8_t byte2 = bytecode[current_frame->local_pc + 2];
@@ -40,12 +69,20 @@ void jsr(){
     return_address.return_address_value = current_frame->local_pc + 3;
     pushValue(&current_frame -> operand_stack, return_address);
     current_frame->local_pc = current_frame->local_pc + branchOffset;
-
 }
 
+/*
+ * Return from subroutine
+ *
+ * opcode:	0xa9
+ * format: 	[ret, index]
+ * stack: 	(...) -> (...)
+ * description:	takes pc value from current frame
+ * and pushes into the operand stack..
+ */
 void ret(){
     Frame* current_frame = peekNode(frame_stack);  
-    int8_t* bytecode = current_frame->current_method->attributes->code_info.code;
+    uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
     int8_t index = bytecode[current_frame->local_pc + 1];
 
     JavaType value ;
@@ -53,6 +90,16 @@ void ret(){
     current_frame->local_pc = current_frame->local_pc + value.return_address_value;
 }
 
+
+/*
+ * Return void from method
+ *
+ * opcode:	0xb1
+ * format: 	[return]
+ * stack: 	(...) -> (empty)
+ * description:	takes previous frame from frame stack
+ * and pushes into the operand stack..
+ */
 void ireturn(){
 	JavaType value;
 	Frame* current_frame = peekNode(frame_stack);
@@ -63,6 +110,16 @@ void ireturn(){
 	Frame* previous_frame = peekNode(frame_stack);
 	pushValue(&previous_frame -> operand_stack, value);
 }
+
+/*
+ * Return void from method
+ *
+ * opcode:	0xb1
+ * format: 	[return]
+ * stack: 	(...) -> (empty)
+ * description:	takes previous frame from frame stack
+ * and pushes into the operand stack..
+ */
 void lreturn(){
 	JavaType value;	
 	Frame* current_frame = peekNode(frame_stack);
@@ -73,6 +130,16 @@ void lreturn(){
 	Frame* previous_frame = peekNode(frame_stack);
 	pushValue(&previous_frame -> operand_stack, value);
 }
+
+/*
+ * Return void from method
+ *
+ * opcode:	0xb1
+ * format: 	[return]
+ * stack: 	(...) -> (empty)
+ * description:	takes previous frame from frame stack
+ * and pushes into the operand stack..
+ */
 void freturn(){
 	JavaType value;	
 	Frame* current_frame = peekNode(frame_stack);
@@ -83,6 +150,16 @@ void freturn(){
 	Frame* previous_frame = peekNode(frame_stack);
 	pushValue(&previous_frame -> operand_stack, value);
 }
+
+/*
+ * Return void from method
+ *
+ * opcode:	0xb1
+ * format: 	[return]
+ * stack: 	(...) -> (empty)
+ * description:	takes previous frame from frame stack
+ * and pushes into the operand stack..
+ */
 void dreturn(){
 	JavaType value;	
 	Frame* current_frame = peekNode(frame_stack);
@@ -93,6 +170,16 @@ void dreturn(){
 	Frame* previous_frame = peekNode(frame_stack);
 	pushValue(&previous_frame -> operand_stack, value);
 }
+
+/*
+ * Return void from method
+ *
+ * opcode:	0xb1
+ * format: 	[return]
+ * stack: 	(...) -> (empty)
+ * description:	takes previous frame from frame stack
+ * and pushes into the operand stack..
+ */
 void areturn(){
 	JavaType value;	
 	Frame* current_frame = peekNode(frame_stack);
@@ -104,8 +191,128 @@ void areturn(){
 	pushValue(&previous_frame -> operand_stack, value);
 }
 
+/*
+ * Return void from method
+ *
+ * opcode:	0xb1
+ * format: 	[return]
+ * stack: 	(...) -> (empty)
+ * description:	takes previous frame from frame stack
+ * and pushes into the operand stack..
+ */
 void return_op(){
-	popNode(&frame_stack);
-	//free(operand_stack);
-	//free(previous_node);
+  Frame* old_frame = popNode(&frame_stack);
+  free(old_frame);
+}
+
+/*
+ * Access jump table by key match and jump
+ *
+ * opcode:	0xab
+ * format: 	[lookupswitch, bytepad, defaultbyte1, defaultbyte2,
+			defaultbyte3, defaultbyte4, npairs1, npairs2, npairs3, npairs4,
+			match-offset pairs]
+ * stack: 	(..., key) -> (...)
+ * description:	takes pc value from current frame
+ * and update using default or offset value..
+ */
+
+void lookupswitch(){
+    JavaType key;	
+	Frame* current_frame = peekNode(frame_stack);
+	popValue(&current_frame -> operand_stack, &key);
+	uint32_t address = current_frame->local_pc;
+
+	current_frame->local_pc++;
+	while((current_frame->local_pc)% 4 != 0){
+		current_frame->local_pc++;	
+	}
+
+	uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
+
+    int8_t byte1 = bytecode[current_frame->local_pc++];
+    int8_t byte2 = bytecode[current_frame->local_pc++];
+    int8_t byte3 = bytecode[current_frame->local_pc++];
+    int8_t byte4 = bytecode[current_frame->local_pc++];
+    int16_t default_value = (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4;
+
+    int8_t byte5 = bytecode[current_frame->local_pc++];
+    int8_t byte6 = bytecode[current_frame->local_pc++];
+    int8_t byte7 = bytecode[current_frame->local_pc++];
+    int8_t byte8 = bytecode[current_frame->local_pc++];    
+    int16_t pairs = (byte5 << 24) | (byte6 << 16) | (byte7 << 8) | byte8;
+
+    for(int i = 0; i < pairs; i++){
+		int8_t match_byte1 = bytecode[current_frame->local_pc++];
+		int8_t match_byte2 = bytecode[current_frame->local_pc++];
+		int8_t match_byte3 = bytecode[current_frame->local_pc++];
+		int8_t match_byte4 = bytecode[current_frame->local_pc++]; 
+        int32_t match = (match_byte1 << 24) | (match_byte2 << 16) |(match_byte3 << 8) | match_byte4;
+
+		int8_t offset_byte1 = bytecode[current_frame->local_pc++];
+		int8_t offset_byte2 = bytecode[current_frame->local_pc++];
+		int8_t offset_byte3 = bytecode[current_frame->local_pc++];
+		int8_t offset_byte4 = bytecode[current_frame->local_pc++]; 
+        int32_t offset = (offset_byte1 << 24) | (offset_byte2 << 16) | (offset_byte3 << 8) | offset_byte4;
+
+        if ((int32_t)key.int_value == match)
+			current_frame->local_pc = address + offset;
+    }
+	current_frame->local_pc = address + default_value;
+}
+
+/*
+ * Access jump table by index and jump
+ *
+ * opcode:	0xaa
+ * format: 	[tableswitch, bytepad, defaultbyte1, defaultbyte2,
+			defaultbyte3, defaultbyte4, lowbyte1, lowbyte2, lowbyte3,
+			lowbyte4, highbyte1, highbyte2, highbyte3, highbyte4, jump offsets]
+ * stack: 	(..., index) -> (...)
+ * description:	takes pc value from current frame
+ * and update using default or offset value..
+ */ 
+
+void tableswitch(){
+    JavaType key;	
+	Frame* current_frame = peekNode(frame_stack);
+	popValue(&current_frame -> operand_stack, &key);
+	uint32_t address = current_frame->local_pc;
+
+	current_frame->local_pc++;
+	while((current_frame->local_pc)% 4 != 0){
+		current_frame->local_pc++;	
+	}
+
+	uint8_t* bytecode = current_frame->current_method->attributes->code_info.code;
+
+    int8_t byte1 = bytecode[current_frame->local_pc++];
+    int8_t byte2 = bytecode[current_frame->local_pc++];
+    int8_t byte3 = bytecode[current_frame->local_pc++];
+    int8_t byte4 = bytecode[current_frame->local_pc++];
+    int16_t default_value = (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4;
+
+    int8_t low_byte1 = bytecode[current_frame->local_pc++];
+    int8_t low_byte2 = bytecode[current_frame->local_pc++];
+    int8_t low_byte3 = bytecode[current_frame->local_pc++];
+    int8_t low_byte4 = bytecode[current_frame->local_pc++];    
+    int16_t low_bytes = (low_byte1 << 24) | (low_byte2 << 16) | (low_byte3 << 8) | low_byte4;
+
+	int8_t high_bytes1 = bytecode[current_frame->local_pc++];
+	int8_t high_bytes2 = bytecode[current_frame->local_pc++];
+	int8_t high_bytes3 = bytecode[current_frame->local_pc++];
+	int8_t high_bytes4 = bytecode[current_frame->local_pc++]; 
+	int32_t high_bytes = (high_bytes1 << 24) | (high_bytes2 << 16) |(high_bytes3 << 8) | high_bytes4;
+
+	for(int match = low_bytes; match <= high_bytes; match++){
+		int8_t offset_byte1 = bytecode[current_frame->local_pc++];
+		int8_t offset_byte2 = bytecode[current_frame->local_pc++];
+		int8_t offset_byte3 = bytecode[current_frame->local_pc++];
+		int8_t offset_byte4 = bytecode[current_frame->local_pc++]; 
+        int32_t offset = (offset_byte1 << 24) | (offset_byte2 << 16) | (offset_byte3 << 8) | offset_byte4;
+
+        if ((int32_t)key.int_value == match)
+			current_frame->local_pc = address + offset;
+    }
+	current_frame->local_pc = address + default_value;
 }
