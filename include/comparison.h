5e70c7ccd90cd3fc80773b03fb9831e06be42aae
guioliunb <52641472+guioliunb@users.noreply.github.com>
2021-04-23 20:09:13 -0300

    lib com assinatura das novas função :§

---
+++ include/comparison.h
@@ -0,0 +1,26 @@
+#ifndef __COMPARISON_H
+#define __COMPARISON_H
+
+#include "common.h"
+
+void lcmp();
+void fcmpl();
+void fcmpg();
+void dcmpl();
+void dcmpg();
+void ifeq();
+void ifne();
+void iflt();
+void ifge();
+void ifgt();
+void ifle();
+void if_icmpeq();
+void if_icmpne();
+void if_icmplt();
+void if_icmpge();
+void if_icmpgt();
+void if_icmple();
+void if_acmpeq();
+void if_acmpne();
+
+#endif  // __COMPARISON_H
